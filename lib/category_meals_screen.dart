726a3587ec1f21b7548ce3daa293c5935dda6b8e
Radi Rusadi <radi.rusadi.s@gmail.com>
2021-05-18 13:46:06 +0700

    Updated dummy data & display list of category meals

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/dummy_data.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
   static const String routeName = '/category-meals';
@@ -11,13 +12,19 @@ class CategoryMealsScreen extends StatelessWidget {
         ModalRoute.of(context).settings.arguments as Map<String, String>;
     final title = routeArgs['title'];
     final id = routeArgs['id'];
+    final categoryMeals = DUMMY_MEALS.where((element) {
+      return element.categories.contains(id);
+    }).toList();
 
     return Scaffold(
       appBar: AppBar(
         title: Text(title),
       ),
-      body: Center(
-        child: Text('The recipes $id'),
+      body: ListView.builder(
+        itemCount: categoryMeals.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Text(categoryMeals[index].title);
+        },
       ),
     );
   }
